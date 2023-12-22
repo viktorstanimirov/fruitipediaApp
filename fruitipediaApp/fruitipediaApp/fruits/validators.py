683b4a67f99b5d7ec44480902a439f5a193b4283
Viktor Stanimirov <122106162+viktorstanimirov@users.noreply.github.com>
2023-12-22 14:12:19 +0200

    Delete fruitipediaApp directory

--- fruitipediaApp/fruitipediaApp/fruits/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_name(value):
-    if not value.isalpha():
-        raise ValidationError("Fruit name should contain only letters!")
